6492c9a5920ec14c0ca052a7706fd9a3d3cab232
victorgxbriel <victorgabrielcn@hotmail.com>
2025-01-20 21:16:44 -0300

    hashtable para funcoes e tipos

---
+++ includes/function_hashtable.c
@@ -0,0 +1,97 @@
+#include "function_table.h"
+#include <string.h>
+#include <stdio.h>
+
+#define TABLE_SIZE 100
+
+unsigned int function_hash(const char *key) {
+    unsigned int hash = 0;
+    for (int i = 0; key[i] != '\0'; i++) {
+        hash = 31 * hash + key[i];
+    }
+    return hash % TABLE_SIZE;
+}
+
+function_table *function_table_create() {
+    function_table *table = malloc(sizeof(function_table));
+    table->entries = malloc(sizeof(function_entry *) * TABLE_SIZE);
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table->entries[i] = NULL;
+    }
+    return table;
+}
+
+function_entry *function_table_pair(const char *id, const char *return_type, char **parameters, int param_count) {
+    function_entry *entry = malloc(sizeof(function_entry));
+    entry->key = strdup(id);
+    entry->return_type = strdup(return_type);
+
+    entry->parameters = malloc(sizeof(char *) * param_count);
+    for (int i = 0; i < param_count; i++) {
+        entry->parameters[i] = strdup(parameters[i]);
+    }
+    entry->param_count = param_count;
+    entry->next = NULL;
+    return entry;
+}
+
+void function_table_set(function_table *table, const char *id, const char *return_type, char **parameters, int param_count) {
+    unsigned int index = function_hash(id);
+    function_entry *entry = table->entries[index];
+
+    while (entry) {
+        if (strcmp(entry->key, id) == 0) {
+            free(entry->return_type);
+            entry->return_type = strdup(return_type);
+
+            for (int i = 0; i < entry->param_count; i++) {
+                free(entry->parameters[i]);
+            }
+            free(entry->parameters);
+
+            entry->parameters = malloc(sizeof(char *) * param_count);
+            for (int i = 0; i < param_count; i++) {
+                entry->parameters[i] = strdup(parameters[i]);
+            }
+            entry->param_count = param_count;
+            return;
+        }
+        entry = entry->next;
+    }
+
+    function_entry *new_entry = function_table_pair(id, return_type, parameters, param_count);
+    new_entry->next = table->entries[index];
+    table->entries[index] = new_entry;
+}
+
+function_entry *function_table_get(function_table *table, const char *id) {
+    unsigned int index = function_hash(id);
+    function_entry *entry = table->entries[index];
+    while (entry) {
+        if (strcmp(entry->key, id) == 0) {
+            return entry;
+        }
+        entry = entry->next;
+    }
+    return NULL;
+}
+
+void function_table_free(function_table *table) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        function_entry *entry = table->entries[i];
+        while (entry) {
+            function_entry *temp = entry;
+            free(entry->key);
+            free(entry->return_type);
+            for (int j = 0; j < entry->param_count; j++) {
+                free(entry->parameters[j]);
+            }
+            free(entry->parameters);
+            entry = entry->next;
+            free(temp);
+        }
+    }
+    free(table->entries);
+    free(table);
+}
---
+++ includes/type_hashtable.c
@@ -0,0 +1,91 @@
+#include "type_table.h"
+#include <string.h>
+#include <stdio.h>
+
+#define TABLE_SIZE 100
+
+unsigned int type_hash(const char *key) {
+    unsigned int hash = 0;
+    for (int i = 0; key[i] != '\0'; i++) {
+        hash = 31 * hash + key[i];
+    }
+    return hash % TABLE_SIZE;
+}
+
+type_table *type_table_create() {
+    type_table *table = malloc(sizeof(type_table));
+    table->entries = malloc(sizeof(type_entry *) * TABLE_SIZE);
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table->entries[i] = NULL;
+    }
+    return table;
+}
+
+type_entry *type_table_pair(const char *type_name, char **attributes, int attribute_count) {
+    type_entry *entry = malloc(sizeof(type_entry));
+    entry->key = strdup(type_name);
+    entry->attributes = malloc(sizeof(char *) * attribute_count);
+    for (int i = 0; i < attribute_count; i++) {
+        entry->attributes[i] = strdup(attributes[i]);
+    }
+    entry->attribute_count = attribute_count;
+    entry->next = NULL;
+    return entry;
+}
+
+void type_table_set(type_table *table, const char *type_name, char **attributes, int attribute_count) {
+    unsigned int index = type_hash(type_name);
+    type_entry *entry = table->entries[index];
+
+    while (entry) {
+        if (strcmp(entry->key, type_name) == 0) {
+            for (int i = 0; i < entry->attribute_count; i++) {
+                free(entry->attributes[i]);
+            }
+            free(entry->attributes);
+
+            entry->attributes = malloc(sizeof(char *) * attribute_count);
+            for (int i = 0; i < attribute_count; i++) {
+                entry->attributes[i] = strdup(attributes[i]);
+            }
+            entry->attribute_count = attribute_count;
+            return;
+        }
+        entry = entry->next;
+    }
+
+    type_entry *new_entry = type_table_pair(type_name, attributes, attribute_count);
+    new_entry->next = table->entries[index];
+    table->entries[index] = new_entry;
+}
+
+type_entry *type_table_get(type_table *table, const char *type_name) {
+    unsigned int index = type_hash(type_name);
+    type_entry *entry = table->entries[index];
+    while (entry) {
+        if (strcmp(entry->key, type_name) == 0) {
+            return entry;
+        }
+        entry = entry->next;
+    }
+    return NULL;
+}
+
+void type_table_free(type_table *table) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        type_entry *entry = table->entries[i];
+        while (entry) {
+            type_entry *temp = entry;
+            free(entry->key);
+            for (int j = 0; j < entry->attribute_count; j++) {
+                free(entry->attributes[j]);
+            }
+            free(entry->attributes);
+            entry = entry->next;
+            free(temp);
+        }
+    }
+    free(table->entries);
+    free(table);
+}
